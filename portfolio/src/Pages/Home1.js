71869b9b9faed110f2d7ff99f337ea36f103eeac
osa623 <it22170798@my.sliit.lk>
2024-07-20 00:15:08 +0530

    mobile responsiveness configs

--- portfolio/src/Pages/Home1.js
+++ portfolio/src/Pages/Home1.js
@@ -443,7 +443,7 @@ const [CounterOn, setCounterOn] = useState(false);
                                       <h2 className='flex text-secondary text-7xl font-russoone text-center'>{CounterOn && <Countup start={0} end={44000} duration={2} delay={0.2}/>}</h2>
                                       <h2 className='flex text-secondary text-7xl font-russoone text-center'>+</h2>
                                     </div>
-                                    <h2 className='flex text-[#3a3a3a] text-6xl font-russoone font-bold text-center'>Followers</h2>
+                                    <h2 className='flex text-[#3a3a3a] text-6xl font-russoone font-bold text-center'>Supporters</h2>
                                     <h2 className='flex text-secondary w-[75vw] text-2xl font-londrina font-light text-center'>Have gathered from all the main and popular platforms</h2>
                               </div>
                               <div className='flex h-[10vh] w-auto items-center justify-center space-x-5'>
@@ -491,7 +491,7 @@ const [CounterOn, setCounterOn] = useState(false);
                                       <h2 className='flex text-secondary text-7xl font-russoone text-center'>{CounterOn && <Countup start={0} end={44000} duration={2} delay={0.2}/>}</h2>
                                       <h2 className='flex text-secondary text-7xl font-russoone text-center'>+</h2>
                                     </div>
-                                    <h2 className='flex text-[#3a3a3a] text-6xl font-russoone font-bold text-center'>Followers</h2>
+                                    <h2 className='flex text-[#3a3a3a] text-4xl font-russoone font-bold text-center'>Supporters</h2>
                                     <h2 className='flex text-secondary w-[22vw] text-2xl font-kanit text-center'>Have gathered from all the main and popular platforms</h2>
                               </div>
                               <div className='flex flex-col h-auto w-[15vw] items-center justify-center space-y-2'>
@@ -503,24 +503,35 @@ const [CounterOn, setCounterOn] = useState(false);
                               </div>
                           </div>
                         </div>
-              </div>                  
+              </div>  
+                              
       </section>
     </ScrollTrigger> 
 
 
-    <section className='relative h-[150vh] max-w-full overflow-hidden'>
-            <div className='flex flex-col h-screen w-full'>
-
-                    
-                    <div className='absolute bottom-0 max-w-full h-auto z-30'>
-                      <img src={moonbg} alt=''/>
-                    </div>
+    <section className='relative  h-[140vh] max-w-full  overflow-hidden'>
+            <div className='flex flex-col h-[120vh] w-full'>   
+              
+              {/*medium and larger screen responsiveness */}
+                    <div className='absolute hidden md:flex bottom-0 max-w-full h-auto z-30'>
+                      <img src={moonbg} alt='' style={{
+                        
+                        
+                      }}/>
+                       </div>
+                       <div className='absolute sm:hidden flex bottom-0 max-w-full h-auto z-30'>
+                      <img src={moonbg} alt='' style={{
+                        width:'auto',
+                        height:'350px'
+                        
+                      }}/>
+                       </div>
                     <div className='absolute flex-col bottom-0 bg-gradient-to-t from-secondary to-transparent w-full h-[60vh] z-30'>
                           <div className='flex flex-col w-full h-[50vh] bg-transparent items-center justify-end mt-50'>
                                       <img src={namelogo} alt='' style={{
-                                      width:'400px',
 
-                                      }}/>
+
+                                      }} className='w-80 md:max-w-xl  cursor-pointer'/>
 
                                   <div className='flex h-[5vh] w-[25vw] items-center justify-center bg-transparent space-x-5'>
 
@@ -528,7 +539,7 @@ const [CounterOn, setCounterOn] = useState(false);
                                                 <svg
                                                   xmlns="http://www.w3.org/2000/svg"
                                                   fill="currentColor"
-                                                  class="text-gray-400"
+                                                  class="text-gray-400  hover:scale-150 transition-transform"
                                                   viewBox="0 0 320 512">
                                                   <path
                                                     d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
@@ -538,7 +549,7 @@ const [CounterOn, setCounterOn] = useState(false);
                                                 <svg
                                                   xmlns="http://www.w3.org/2000/svg"
                                                   fill="currentColor"
-                                                  class="text-gray-400"
+                                                  class="text-gray-400  hover:scale-150 transition-transform"
                                                   viewBox="0 0 448 512">
                                                       <path
                                                     d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
@@ -548,7 +559,7 @@ const [CounterOn, setCounterOn] = useState(false);
                                                     <svg
                                                       xmlns="http://www.w3.org/2000/svg"
                                                       fill="currentColor"
-                                                      class="text-gray-400"
+                                                      class="text-gray-400  hover:scale-150 transition-transform"
                                                       viewBox="0 0 448 512">
                                                       <path
                                                         d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z" />
@@ -568,7 +579,7 @@ const [CounterOn, setCounterOn] = useState(false);
                                                         <svg
                                                           xmlns="http://www.w3.org/2000/svg"
                                                           fill="currentColor"
-                                                 class="text-gray-400"
+                                                          class="text-gray-400  hover:scale-150 transition-transform"
                                                           viewBox="0 0 496 512">
                                                           <path
                                                             d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
@@ -576,27 +587,36 @@ const [CounterOn, setCounterOn] = useState(false);
                                          </span>
 
                                   </div>
-                                  <div className='flex w-[60vw] h-auto bg-transparent items-center justify-center'>
-                                    <h2 className='text-primary text-lg pt-5 font-mono font-thin'>2024 Osanda Muthukumarana. All rights reserved.</h2>
-                                  </div>
+                                  <div className='flex w-full sm:w-[40vw] md:w-[60vw] h-auto bg-transparent items-center justify-center'>
+                                  <h2 className='text-primary text-center pt-5 font-mono font-thin'>
+                                    2024{' '}<span className='font-russoone cursor-pointer'>Osanda Muthukumarana.</span>{''} All rights reserved.
+                                  </h2>
+                                </div>
+
                           </div>
                     </div>
-                    <div className='absolute bottom-5 max-w-full h-auto z-20'>
+                    <div className='absolute hidden md:flex bottom-5 max-w-full h-auto z-20'>
                       <img src={moonbackground} alt=''/>
+                    </div>
+                    <div className='absolute  sm:hidden flex bottom-0 max-w-full h-auto z-20'>
+                      <img src={moonbackground} alt='' style={{
+                        width:'auto',
+                        height:'380px'
+                      }}/>
                     </div>
 
                         <motion.div 
                         initial={{opacity:'0'}}
                         whileInView={{opacity:'100'}}
                         transition={{duration:'4.0', ease:'easeInOut', delay:'5.0'}}
-                        className="absolute flex top-2 left-5  w-auto h-[40vh] z-40 overflow-hidden">
-                           <div className='flex bg-primary w-[60vw] h-[30vh] rounded-full items-center p-10 justify-center space-x-3'>
+                        className="absolute hidden md:flex top-2 left-5  lg:w-auto h-[40vh] z-40 overflow-hidden">
+                           <div className=' bg-primary flex  w-auto lg:h-[30vh]  md:h-[25vh] rounded-full items-center p-10 justify-center space-x-3'>
 
-                           <p className='flex w-[15vw] h-auto text-3xl font-londrina  cursor-default'>If you appreciate my Designing works and find it valuable,consider</p>
-                           <p className='flex w-[10vw] h-auto text-5xl font-londrina bg-[#ffb700] rounded-3xl text-center text-[#613215] cursor-pointer p-0'><a href='https://buymeacoffee.com/osandamuthukumarana'>Buy me a
+                           <p className='flex lg:w-[15vw] h-auto lg:text-2xl md:text-xl font-londrina  cursor-default'>If you appreciate my Designing works and find it valuable,consider</p>
+                           <p className='flex lg:w-[10vw] max:lg:h-[25vh] md:h-[20vh] lg:text-3xl md:text-2xl font-londrina lg:p-2 bg-[#ffb700] rounded-3xl text-center text-[#613215] cursor-pointer p-0'><a href='https://buymeacoffee.com/osandamuthukumarana'>Buy me a
                            <FontAwesomeIcon icon={faCoffee} className='mx-5 size-11 rounded-full bg-primary border-8' />
                            Coffee</a></p>
-                           <p className='flex w-[35vw] h-auto text-3xl font-londrina  cursor-default'>Your support fuels my creativity and helps me continue crafting and sharing high-quality projects. Thank you for your generosity!</p>
+                           <p className='flex lg:w-[35vw] md:w-[50vw] h-auto  lg:text-2xl md:text-xl font-londrina  cursor-default'>Your support fuels my creativity and helps me continue crafting and sharing high-quality projects. Thank you for your generosity!</p>
                            </div>
                            <div className='flex w-[18vw] h-auto rounded-3xl' style={{
                             backgroundImage:`url(${quoteimg})`,
@@ -609,11 +629,13 @@ const [CounterOn, setCounterOn] = useState(false);
                         </motion.div>
 
 
+
+
                          <motion.div
                           initial={{right:'-500px'}}
                           whileInView={{right:'30px'}}
                           transition={{duration:'4.0',ease:'easeInOut', delay:'0.3'}}
-                          className='absolute max-w-full h-auto top-40 animate-bounce' style={{
+                          className='absolute hidden md:flex max-w-full h-auto top-40  animate-bounce' style={{
                             animationDuration:'20s'
                           }}>
                             <img src={astronut} alt='' style={{
@@ -622,13 +644,27 @@ const [CounterOn, setCounterOn] = useState(false);
                             }}/>
                           </motion.div>
 
+                          <motion.div
+                          initial={{right:'-300px'}}
+                          whileInView={{right:'0px'}}
+                          transition={{duration:'4.0',ease:'easeInOut', delay:'0.3'}}
+                          className='absolute sm:hidden flex max-w-full h-auto top-80  animate-bounce' style={{
+                            animationDuration:'40s'
+                          }}>
+                            <img src={astronut} alt='' style={{
+                              width:'300px',
+                              rotatez:'120deg',
+                            }}/>
+                          </motion.div>
+
+
 
 
                           <motion.div
-                          initial={{bottom:'-500px'}}
+                          initial={{bottom:'-400px'}}
                           whileInView={{bottom:'50px'}}
                           transition={{duration:'3.0',ease:'easeInOut', delay:'0.3'}}
-                          className='absolute max-w-full h-auto animate-spin animate-duration-300' style={{
+                          className='absolute min-w-sm h-auto animate-spin animate-duration-300 lg:max-w-4xl lg:min-w-2xl md:max-w-2xl' style={{
                             animationDuration:'400s'
                           }}>
                             <img src={earthimage} alt='' style={{
@@ -639,6 +675,37 @@ const [CounterOn, setCounterOn] = useState(false);
 
                                 
             </div>  
+            
+            <motion.div
+                          initial={{right:'-400px'}}
+                          whileInView={{right:'0px'}}
+                          transition={{duration:'4.0',ease:'easeInOut', delay:'0.3'}}
+                          className='absolute sm:hidden flex max-w-full h-auto top-20 p-5  animate-bounce' style={{
+                            animationDuration:'40s'
+                          }}>
+                       <div className='flex flex-col h-auto w-auto'>   
+                        <div className='flex bg-primary h-auto w-[90vw] rounded-xl items-center justify-center p-2 space-x-2 z-30'>
+                                  
+                           <p className='flex w-auto h-[10vh] font-londrina cursor-default text-center' style={{
+                            fontSize:'12px'
+                           }}>If you appreciate my Designing works and find it valuable,consider</p>
+                           <p className='flex font-londrina w-auto h-[10vh] text-sm  bg-[#ffb700] rounded-3xl text-center text-[#613215] cursor-pointer p-0'><a href='https://buymeacoffee.com/osandamuthukumarana'>Buy me a
+                           <FontAwesomeIcon icon={faCoffee} className='mx-5 size-5 rounded-full bg-primary border-8' />
+                           Coffee</a></p>
+                           <p className='flex  font-londrina  w-auto h-[10vh]  cursor-default' style={{
+                            fontSize:'12px'
+                           }}>Your support fuels my creativity and helps me continue crafting and sharing high-quality projects. Thank you for your generosity!</p>
+    
+                              </div>
+                              <div className='flex w-[60vw] h-[20vh] ml-20 rounded-3xl' style={{
+                            backgroundImage:`url(${quoteimg})`,
+                            backgroundPosition:'center',
+                            backgroundSize:'cover',
+                            rotate:'10deg',}}>
+                           </div>
+                        </div>  
+
+                          </motion.div>
  
 
     </section>
